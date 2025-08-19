812f327b363b0c04e006388150df8bda5ef02660
Luis Flores <luisfloresvid@gmail.com>
2025-08-19 14:31:08 -0700

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
@@ -31,6 +32,7 @@ export default function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
